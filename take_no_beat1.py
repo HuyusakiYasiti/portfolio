91f20b1cad8aec00deb724e0350e49f4e228a39c
CROW_07 <100528047+HuyusakiYasiti@users.noreply.github.com>
2022-12-11 00:33:24 +0900

    Update take_no_beat1.py

--- take_no_beat1.py
+++ take_no_beat1.py
@@ -8,10 +8,13 @@ n = input("Please enter an integer value.: ")
 n = int(n)
 
 
+#カウントダウン
+
 for i in range(n, 0, -1):
 
     print(i)
 
     time.sleep(random.triangular(0.5, 1.5, 1.0))
+
 
 print("complete")
